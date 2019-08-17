a4a61660e816eb94c0c54c212aa4e89cd03b891d
kaiorosa1 <kaiosrosa@gmail.com>
2019-08-16 22:37:24 -0300

    add Ponto Class

---
+++ ReviewProject/Ponto.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Ponto.cpp
+ * Author: user
+ * 
+ * Created on 16 de Agosto de 2019, 22:36
+ */
+
+#include "Ponto.h"
+
+Ponto::Ponto() {
+}
+
+Ponto::Ponto(const Ponto& orig) {
+}
+
+Ponto::~Ponto() {
+}
+
--- ReviewProject/nbproject/Makefile-Debug.mk
+++ ReviewProject/nbproject/Makefile-Debug.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/Ponto.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/reviewproject.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/reviewproject ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/Ponto.o: Ponto.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Ponto.o Ponto.cpp
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
