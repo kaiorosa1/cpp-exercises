b6a0101f06f16ee9ecaedaed8b2a2b62005d1fea
kaiorosa1 <kaiosrosa@gmail.com>
2019-08-18 15:21:30 -0300

    add more reviewing material cpp
    -template functions
    -custom namespace

--- ReviewProject/main.cpp
+++ ReviewProject/main.cpp
@@ -15,21 +15,33 @@
 #include <iostream>
 #include "math_utils.h"
 
+// custom namespace
+namespace test {
+    // templatetize a function
+    template <typename T>
+    void swap(T &a, T &b){
+        T temp =a;
+        a = b;
+        b= temp;   
+    }
+}
+
+
 int main(int argc, char** argv) {
     
     // REVIEW BASIC CONCEPTS
     // how to print stuff out c++ style
     std::cout << "This is only a test" << std::endl;
     // how to get input from the user
-    int nTimes;
-    std:: cout << "How many times you said?" << std::endl;
-    std::cin >> nTimes; // to get input from the user use the cin obj from the std (namespace?)
-    for(int i =0;i< nTimes;i++){
-        std::cout<< i+1 << " ";
-    }
-    std::cout<< std::endl;
+//    int nTimes;
+//    std:: cout << "How many times you said?" << std::endl;
+//    std::cin >> nTimes; // to get input from the user use the cin obj from the std (namespace?)
+//    for(int i =0;i< nTimes;i++){
+//        std::cout<< i+1 << " ";
+//    }
+//    std::cout<< std::endl;
     // custom functions 
-    print_name();
+//    print_name();
     // function overloading example
     Rectangle rect;
     rect.length = 10;
@@ -43,8 +55,27 @@ int main(int argc, char** argv) {
     // default parameters
     std::cout << pow(4,3) << std::endl;
     std::cout<< pow(4) << std::endl;
+    
+    // swap the values 
+    std::string first_name = "Kaio";
+    std::string last_name = "Rosa";
+    
+    std::cout << "Names:"<< std::endl;
+    std::cout << first_name << " "<< last_name<< std::endl; 
+    // swapping the names
+    test::swap(first_name,last_name);
+    std::cout << "Names Swapped!"<< std::endl;
+    std::cout << first_name << " "<< last_name<< std::endl;
+    
+    int numbers[] = {2,3};
+    std::cout << "Numbers:"<< std::endl;
+    std::cout << numbers[0] << " "<< numbers[1]<< std::endl;
+    // swapping numbers
+    test::swap(numbers[0],numbers[1]);
+    std::cout << "Numbers Swapped!"<< std::endl;
+     std::cout << numbers[0] << " "<< numbers[1]<< std::endl;
+     
+     
     return 0;
 }
-// in case any value is assigned to exponent the default value is two
-
 
